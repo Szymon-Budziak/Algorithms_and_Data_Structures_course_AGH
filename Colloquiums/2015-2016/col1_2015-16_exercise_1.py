ad400d45964d3e105505b59005487f77addddf57
szymon-budziak <szymonbudziak@gmail.com>
2021-04-01 23:13:12 +0200

    Colloquium 1 2015-16 exercise 1 revised

--- Colloquiums/2015-2016/col1_2015-16_exercise_1.py
+++ Colloquiums/2015-2016/col1_2015-16_exercise_1.py
@@ -8,10 +8,10 @@ from random import randint
 
 
 def partition(T, p, r):
-    pivot = T[r][1]
+    pivot = T[r][2]
     i = p-1
     for j in range(p, r):
-        if T[j][1] <= pivot:
+        if T[j][2] <= pivot:
             i += 1
             T[i], T[j] = T[j], T[i]
     T[i+1], T[r] = T[r], T[i+1]
@@ -30,7 +30,7 @@ def sum_sort(A, B, n):
     result = current = start = j = i = 0
     while i <= len(A):
         if current == n:
-            C[j] = (A[start:i], result)
+            C[j] = (start, i, result)
             current = 1
             if i != len(A):
                 result = A[i]
@@ -40,11 +40,22 @@ def sum_sort(A, B, n):
             current += 1
             result += A[i]
         i += 1
+    # Complexity of while loop which checks for the lowest n-sum up to the
+    # biggest m-sum is O(n^2)
     quicksort(C, 0, n-1)
+    # Expected complexity of Quicksort is O(n*log(n))
+    start_b = 0
+    end_b = n
+    for i in range(len(C)):
+        B[start_b:end_b] = A[C[i][0]:C[i][1]]
+        start_b += n
+        end_b += n
+    # Rewriting to the B array takes O(n)
+    # The whole algorithm has a complexity of O(n^2)
+    return B
 
 
-n = 4
+n = 5
 A = [randint(1, 100) for _ in range(n**2)]
-print(len(A))
 B = [0] * (n**2)
-sum_sort(A, B, n)
+print(sum_sort(A, B, n))
