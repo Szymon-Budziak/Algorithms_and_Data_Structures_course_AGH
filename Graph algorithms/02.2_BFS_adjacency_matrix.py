3dd22e577eea7bfc548a3a938c96aec8f589d44b
szymon-budziak <szymonbudziak@gmail.com>
2021-05-07 15:08:56 +0200

    BFS adjacency matrix

---
+++ Graph algorithms/02.2_BFS_adjacency_matrix.py
@@ -0,0 +1,27 @@
+# BFS - Breadth First Search
+from queue import Queue
+
+
+def BFS(graph, root):
+    queue = Queue()
+    visited = [False] * len(graph)
+    result = []
+    queue.put(root)
+    visited[root] = True
+    while not queue.empty():
+        u = queue.get()
+        result.append(u)
+        for v in range(len(graph)):
+            if visited[v] is False and graph[u][v]:
+                queue.put(v)
+                visited[v] = True
+    return result
+
+
+graph = [[0, 1, 0, 1, 1],
+         [1, 0, 0, 1, 0],
+         [0, 0, 0, 0, 1],
+         [1, 1, 0, 0, 1],
+         [1, 0, 1, 1, 0]]
+
+print(BFS(graph, 0))
