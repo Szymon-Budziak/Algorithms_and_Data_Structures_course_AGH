d34ad3b2b5e115df681a46a85f57141022815dd3
szymon-budziak <szymonbudziak@gmail.com>
2021-05-10 00:04:06 +0200

    Graph Alg topological sorting

---
+++ Graph algorithms/12_topological_sorting.py
@@ -0,0 +1,24 @@
+# DAG is a directed acyclic graph
+# Topological sorting of DAG consists in arranging the vertices in order that the edges point
+# only from left to right.
+
+
+def dfs(graph, source, visited, result):
+    visited[source] = True
+    for v in graph[source]:
+        if not visited[v]:
+            dfs(graph, v, visited, result)
+    result.insert(0, source)
+
+
+def topological_sorting(graph):
+    visited = [False] * len(graph)
+    result = []
+    for i in range(len(graph)):
+        if not visited[i]:
+            dfs(graph, i, visited, result)
+    return result
+
+
+graph = [[1, 2], [2, 3], [], [4, 5, 6], [], [], [], [3], [7]]
+print(topological_sorting(graph))
