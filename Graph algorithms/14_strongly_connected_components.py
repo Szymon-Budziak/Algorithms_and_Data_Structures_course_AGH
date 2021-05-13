8dc5d224508cc29fcf468aca8f0fe31468355e3d
szymon-budziak <szymonbudziak@gmail.com>
2021-05-13 11:55:41 +0200

    Graph Alg strongly connected components

---
+++ Graph algorithms/14_strongly_connected_components.py
@@ -0,0 +1,43 @@
+# Let G = (V, E) be a directed graph. We say that u,v belong to the same strongly connected
+# component if there are directed paths from u to v and from v to u.
+
+
+def SCC(graph):
+    visited = [False] * len(graph)
+    stack = []
+    DFSUtil(graph, 0, visited, stack)
+    new_graph = [[] for _ in range(len(graph))]
+    transpose_graph(graph, new_graph)
+    for j in range(len(visited)):
+        visited[j] = False
+    while len(stack):
+        u = stack.pop()
+        if not visited[u]:
+            dfs(new_graph, u, visited)
+            print()
+
+
+def dfs(graph, source, visited):
+    visited[source] = True
+    print(source, end=" ")
+    for v in graph[source]:
+        if not visited[v]:
+            dfs(graph, v, visited)
+
+
+def DFSUtil(graph, source, visited, stack):
+    visited[source] = True
+    for v in graph[source]:
+        if not visited[v]:
+            DFSUtil(graph, v, visited, stack)
+    stack.append(source)
+
+
+def transpose_graph(graph, new_graph):
+    for i in range(len(graph)):
+        for j in range(len(graph[i])):
+            new_graph[graph[i][j]].append(i)
+
+
+graph = [[1, 4], [2, 3], [0, 7], [4], [5], [3, 6], [3], [8], [9], [10], [6, 7]]
+SCC(graph)
