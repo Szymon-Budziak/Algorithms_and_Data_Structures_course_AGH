5c3891f4ccc85e95b58ab46b91e4f3e7e37c9d85
szymon-budziak <szymonbudziak@gmail.com>
2021-05-15 19:01:44 +0200

    Graph Alg strongly connected components

--- Graph algorithms/14_strongly_connected_components.py
+++ Graph algorithms/14_strongly_connected_components.py
@@ -10,19 +10,22 @@ def SCC(graph):
     transpose_graph(graph, new_graph)
     for j in range(len(visited)):
         visited[j] = False
+    result = [[] for _ in range(len(graph))]
+    index = 0
     while len(stack):
         u = stack.pop()
         if not visited[u]:
-            dfs(new_graph, u, visited)
-            print()
+            dfs(new_graph, u, visited, result, index)
+            index += 1
+    return result
 
 
-def dfs(graph, source, visited):
+def dfs(graph, source, visited, result, index):
     visited[source] = True
-    print(source, end=" ")
+    result[index].append(source)
     for v in graph[source]:
         if not visited[v]:
-            dfs(graph, v, visited)
+            dfs(graph, v, visited, result, index)
 
 
 def DFSUtil(graph, source, visited, stack):
@@ -40,4 +43,4 @@ def transpose_graph(graph, new_graph):
 
 
 graph = [[1, 4], [2, 3], [0, 7], [4], [5], [3, 6], [3], [8], [9], [10], [6, 7]]
-SCC(graph)
+print(SCC(graph))
