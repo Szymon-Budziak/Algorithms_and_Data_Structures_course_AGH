f03217b8e89a5ef459fd1433ea1303e63c6e5db7
szymon-budziak <szymonbudziak@gmail.com>
2021-05-24 13:09:38 +0200

    Graph Alg find-union

---
+++ Graph algorithms/22_find-union.py
@@ -0,0 +1,28 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.rank = 0
+        self.parent = self
+
+
+def find(x):
+    if x != x.parent:
+        x.parent = find(x.parent)
+    return x.parent
+
+
+def union(x, y):
+    x = find(x)
+    y = find(y)
+    if x == y:
+        return
+    if x.rank > y.rank:
+        y.parent = x
+    else:
+        x.parent = y
+        if x.rank == y.rank:
+            y.rank += 1
+
+
+def make_set(v):
+    return Node(v)
