3fa605484e0d4797bc6f403fb9b8470efef84deb
szymon-budziak <szymonbudziak@gmail.com>
2021-05-25 23:54:44 +0200

    Graph Alg Dijkstra's algorithm adjacency matrix

---
+++ Graph algorithms/28_Dijkstra's_algorithm_adjacency_matrix.py
@@ -0,0 +1,45 @@
+# Dijkstra's algorithm for finding the shortest paths in weighted graph on adjacency matrix.
+from queue import PriorityQueue
+from math import inf
+
+
+def relax(graph, u, v, distance, parent):
+    if distance[v] > distance[u] + graph[u][v]:
+        distance[v] = distance[u] + graph[u][v]
+        parent[v] = u
+        return True
+    return False
+
+
+def dijkstra_algorithm(graph, source):
+    queue = PriorityQueue()
+    queue.put(source)
+    visited = [False] * len(graph)
+    parent = [None] * len(graph)
+    distance = [inf] * len(graph)
+    for i in range(len(graph)):
+        if i != source:
+            queue.put(i)
+    distance[source] = 0
+    while not queue.empty():
+        u = queue.get()
+        if not visited[u]:
+            visited[u] = True
+            for v in range(len(graph[u])):
+                if graph[u][v] != 0:
+                    if relax(graph, u, v, distance, parent):
+                        queue.put(v)
+    return parent, distance
+
+
+graph = [[0, 1, 5, 0, 0],
+         [1, 0, 2, 8, 7],
+         [5, 2, 0, 3, 0],
+         [0, 8, 3, 0, 1],
+         [0, 7, 0, 1, 0]]
+
+parent, distance = dijkstra_algorithm(graph, 0)
+i = len(parent) - 1
+while parent[i] is not None:
+    print(i, distance[i])
+    i -= 1
