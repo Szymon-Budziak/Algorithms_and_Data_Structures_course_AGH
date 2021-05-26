c08517ba414fb2c1ace1d786474f7723eb7d2dc2
szymon-budziak <szymonbudziak@gmail.com>
2021-05-26 15:00:03 +0200

    Graph Alg Dijkstra's algorithm adjacency list

--- Graph algorithms/27_Dijkstra's_algorithm_adjacency_list.py
+++ Graph algorithms/27_Dijkstra's_algorithm_adjacency_list.py
@@ -3,6 +3,9 @@ from queue import PriorityQueue
 from math import inf
 
 
+# 1st solution:
+
+
 def relax(u, v, distance, parent):
     if distance[v[0]] > distance[u] + v[1]:
         distance[v[0]] = distance[u] + v[1]
@@ -31,6 +34,25 @@ def dijkstra_algorithm(graph, source):
     return parent, distance
 
 
+# 2nd solution:
+
+
+def dijkstra_algorithm_2(graph, source):
+    queue = PriorityQueue()
+    distance = [inf] * len(graph)
+    counter = len(graph)
+    queue.put((0, source))
+    while not queue.empty() and counter > 0:
+        dist, v = queue.get()
+        if dist < distance[v]:
+            counter -= 1
+            distance[v] = dist
+            for edge in graph[v]:
+                u, length = edge
+                queue.put((dist + length, u))
+    return distance
+
+
 graph = [[(1, 1), (2, 12)],
          [(0, 1), (2, 7), (3, 5)],
          [(0, 12), (1, 2), (3, 6), (4, 2)],
@@ -42,4 +64,10 @@ parent, distance = dijkstra_algorithm(graph, 0)
 i = len(parent) - 1
 while parent[i] is not None:
     print(i, distance[i])
+    i -= 1
+print("----------")
+distance_2 = dijkstra_algorithm_2(graph, 0)
+i = len(parent) - 1
+while i != 0:
+    print(i, distance_2[i])
     i -= 1
