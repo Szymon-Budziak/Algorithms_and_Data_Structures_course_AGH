d44078d9b5bb79f5b87f559832209173dbd8e851
szymon-budziak <szymonbudziak@gmail.com>
2021-06-09 16:10:56 +0200

    Graph Alg Ford-Fulkerson algorithm (maximum flow)

---
+++ Graph algorithms/46_Ford-Fulkerson_algorithm.py
@@ -0,0 +1,50 @@
+# The Ford-Fulkerson algorithm is a greedy algorithm that computes the maximum flow in a flow network (graph).
+from queue import Queue
+from math import inf
+
+
+def bfs(graph, s, t, parent):
+    queue = Queue()
+    visited = [False] * len(graph)
+    visited[s] = True
+    queue.put(s)
+    while not queue.empty():
+        u = queue.get()
+        for v in range(len(graph)):
+            if not visited[v] and graph[u][v] != 0:
+                visited[v] = True
+                parent[v] = u
+                queue.put(v)
+    return visited[t]
+
+
+def ford_fulkerson_algorithm(graph, s, t):
+    parent = [None] * len(graph)
+    max_flow = 0
+    while bfs(graph, s, t, parent):
+        current_flow = inf
+        current = t
+        while current != s:
+            current_flow = min(current_flow, graph[parent[current]][current])
+            current = parent[current]
+        max_flow += current_flow
+        v = t
+        while v != s:
+            u = parent[v]
+            graph[u][v] -= current_flow
+            graph[v][u] += current_flow
+            v = parent[v]
+    return max_flow
+
+
+graph = [[0, 11, 12, 17, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 14, 0, 0, 0, 0],
+         [0, 0, 0, 0, 8, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 9, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 7, 0, 10, 0],
+         [0, 0, 0, 0, 0, 0, 6, 9, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 5],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 3],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 8],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+print(ford_fulkerson_algorithm(graph, 0, 9))
