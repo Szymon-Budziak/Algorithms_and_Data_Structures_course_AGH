5330caf0a21f258dff7b041de57a206c2c607c4f
szymon-budziak <szymonbudziak@gmail.com>
2021-06-22 15:56:40 +0200

    Sort half of the elements of equal value

--- Sort/12_half_of_the_elements_of_equal_value.py
+++ Sort/12_half_of_the_elements_of_equal_value.py
@@ -1,6 +1,10 @@
 # We are given an array A with a natural numbers. Find algorithm with O(n) complexity which determines
 # whether more than half of the elements int the array have the same value.
 
+
+# 1st solution:
+
+
 def insertion_sort(T):
     for i in range(1, len(T)):
         key = T[i]
@@ -35,5 +39,23 @@ def half(A):
     return False
 
 
+# 2nd solution:
+
+
+def half2(A):
+    maximum = max(A)
+    buckets = [[] for _ in range(len(A) + 1)]
+    for i in range(len(buckets)):
+        buckets[i].append(0)
+    for i in range(len(A)):
+        index = int((A[i] / maximum) * len(A))
+        buckets[index].append(A[i])
+        buckets[index][0] += 1
+        if buckets[index][0] > len(A) // 2:
+            return True
+    return False
+
+
 A = [12, 3, 5, 7, 7, 12, 7, 4, 10, 2, 7, 7, 7, 7, 7]
 print(half(A))
+print(half2(A))
