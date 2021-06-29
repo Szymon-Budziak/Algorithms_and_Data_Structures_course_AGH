7b4d14a87f10efb977061953b58e0820ab89095d
szymon-budziak <szymonbudziak@gmail.com>
2021-06-29 17:56:02 +0200

    GA car fueling

--- Greedy algorithms/02_car_fueling.py
+++ Greedy algorithms/02_car_fueling.py
@@ -1,3 +1,8 @@
+# We are given the distance which is the distance in km to covered by a car on a single road.
+# We are also give stops that are gas station and total capacity of the fuel tank. Each station
+# is defined by its distance from point 0 i.e. s is the distance between the [i]-th station
+# and point 0. Find algorithm that will calculate the minimum number of refills (number of
+# stations the car must stop at) on the entire route.
 
 
 def car_fueling(distance, full_tank, stops):
@@ -7,7 +12,7 @@ def car_fueling(distance, full_tank, stops):
     while limit < distance:
         if current_refill >= len(stops) or stops[current_refill] > limit:
             return False
-        while current_refill < len(stops)-1 and stops[current_refill+1] <= limit:
+        while current_refill < len(stops) - 1 and stops[current_refill + 1] <= limit:
             current_refill += 1
         refills += 1
         limit = stops[current_refill] + full_tank
