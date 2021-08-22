0de61c0e82345f5dcc15db7166bed74370bf1942
szymonbudziak@gmail.com <szymonbudziak@gmail.com>
2021-08-22 16:13:07 +0200

    DP coin change problem

--- Dynamic Programming/05_coin_change_problem.py
+++ Dynamic Programming/05_coin_change_problem.py
@@ -1,19 +1,18 @@
-# We are given an array of numbers that represent each coin. The number of coins
-# we have are infinity, so we do not need to worry about how many coins are
-# at uor disposal. Then we are given an amount and asked to find the minimum
-# number of coins that are needed to make that amount.
+# We are given an array of numbers that represent each coin. The number of coins we have are infinity,
+# so we do not need to worry about how many coins are at our disposal. Then we are given an amount and
+# asked to find how many ways can we make the change.
 
 
-def count_coin_changes(coins, number):
-    ways = [0]*(number+1)
+def count_coin_changes(coins, total_money):
+    ways = [0] * (total_money + 1)
     ways[0] = 1
     for i in range(len(coins)):
         for j in range(len(ways)):
             if coins[i] <= j:
-                ways[j] += ways[j-coins[i]]
-    return ways[number]
+                ways[j] += ways[j - coins[i]]
+    return ways[total_money]
 
 
-number = 11
+total_money = 11
 coins = [1, 2, 10]
-print(count_coin_changes(coins, number))
+print(count_coin_changes(coins, total_money))
